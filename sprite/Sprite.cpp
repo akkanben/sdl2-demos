a36546ac3b1fca3a5488a42d17abac1f1fe0595c
Ben Mills <fuzzyllama@gmail.com>
2022-05-06 16:16:37 -0700

    Adds sprite class for textured rectangles

---
+++ sprite/Sprite.cpp
@@ -0,0 +1,46 @@
+#include <string>
+
+#include <SDL2/SDL.h>
+
+#include "Sprite.hpp"
+
+Sprite::Sprite(SDL_Renderer *renderer, std::string texturePath, int x, int y, int width,
+               int height) {
+  mRenderer = renderer;
+  SDL_Surface *surface = SDL_LoadBMP(texturePath.c_str());
+  mTexture = SDL_CreateTextureFromSurface(mRenderer, surface);
+  SDL_FreeSurface(surface);
+  mRectangle.x = x;
+  mRectangle.y = y;
+  mRectangle.h = height;
+  mRectangle.w = width;
+}
+
+Sprite::~Sprite() { SDL_DestroyTexture(mTexture); }
+
+void Sprite::setPosition(int x, int y) {
+  mRectangle.x = x;
+  mRectangle.y = y;
+}
+
+void Sprite::setSize(int width, int height) {
+  mRectangle.w = width;
+  mRectangle.h = height;
+  if (mRectangle.w < 1)
+    mRectangle.w = 1;
+  if (mRectangle.h < 1)
+    mRectangle.h = 1;
+}
+
+void Sprite::setColorKey(std::string texturePath, Uint8 redKey, Uint8 greenKey, Uint8 blueKey) {
+  SDL_Surface *surface = SDL_LoadBMP(texturePath.c_str());
+  SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, redKey, greenKey, blueKey));
+  mTexture = SDL_CreateTextureFromSurface(mRenderer, surface);
+  SDL_FreeSurface(surface);
+}
+
+int Sprite::getWidth() { return mRectangle.w; }
+
+int Sprite::getHeight() { return mRectangle.h; }
+
+void Sprite::render() { SDL_RenderCopy(mRenderer, mTexture, NULL, &mRectangle); }
